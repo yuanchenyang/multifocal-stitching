8281c1b40b46fb611992db4652e5ab5f941a669e
Chenyang Yuan <yuanchenyang@gmail.com>
2023-07-28 12:17:56 -0400

    Shifted resize into merge

--- src/multifocal_stitching/merge_imgs.py
+++ src/multifocal_stitching/merge_imgs.py
@@ -3,7 +3,8 @@ from PIL import Image
 from typing import Tuple
 from .utils import *
 
-def merge(i1: Image, i2: Image, dx:int, dy:int) -> Tuple[Image.Image, Image.Image]:
+def merge(i1: Image, i2: Image, dx:int, dy:int, resize_factor:int = 1
+          ) -> Tuple[Image.Image, Image.Image]:
     assert i1.size == i2.size, "Images must be same size!"
     W, H = i1.size
     new_W, new_H = W + abs(dx), H + abs(dy)
@@ -20,18 +21,16 @@ def merge(i1: Image, i2: Image, dx:int, dy:int) -> Tuple[Image.Image, Image.Imag
     res_r = res.copy()
     res_r.paste(i1, (i1_x, i1_y))
 
-    return res, res_r
+    return [r.resize((new_W // resize_factor, new_H // resize_factor), Image.LANCZOS)
+            for r in (res, res_r)]
 
 def merge_and_save(base_dir:str, res_dir:str, img_name1:str, img_name2:str,
-                   dx:int, dy:int, resize_factor:int=1, save_gif:bool=False):
+                   dx:int, dy:int, save_gif:bool=False, **kwargs):
     i1, i2 = [Image.open(get_full_path(base_dir, i)) for i in (img_name1, img_name2)]
-    res = merge(i1, i2, dx, dy)
-    W, H = res[0].size
-    res_resized = [r.resize((W // resize_factor, H // resize_factor), Image.LANCZOS)
-                   for r in res]
-    for i, r in enumerate(res_resized):
+    res = merge(i1, i2, dx, dy, **kwargs)
+    for i, r in enumerate(res):
         base1, base2 = [os.path.splitext(n)[0] for n in (img_name1, img_name2)]
         r.save(get_full_path(res_dir, f'{base1}__{base2}_{i}.jpg'))
     if save_gif:
-        res_resized[0].save(get_full_path(res_dir, f'{base1}__{base2}.png'), save_all=True,
-                            append_images=res_resized[1:], duration=500, loop=0)
+        res[0].save(get_full_path(res_dir, f'{base1}__{base2}.png'), save_all=True,
+                            append_images=res[1:], duration=500, loop=0)
